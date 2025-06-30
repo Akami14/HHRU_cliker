72a9653ed52fd1b1fdfdba42c9ba3dd80a157737
Akami14 <98305306+Akami14@users.noreply.github.com>
2025-06-30 16:35:10 +0300

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from hhru_func_2 import loggin_and_click
+
+
+
+if __name__ == "__main__":
+    login = '9851149815'
+
+    url_2 = 'https://vladimir.hh.ru/search/vacancy?from=resumelist&education=not_required_or_not_specified&resume=2b455687ff0b4d5c470039ed1f43475a586f6c&search_field=name&search_field=description&search_field=company_name&work_format=REMOTE&enable_snippets=false&forceFiltersSaving=true'
+    driver = webdriver.Firefox()
+    loggin_and_click(driver, login, url_2)
+    #print(window_handles)
+    #vacancy_cliks(url_2)
+    page_name = '&page='
+
+
+    for p_number in range(40):
+        print(p_number)
+        next_page = url_2 + page_name+str(p_number)
+        #print(driver.getWindowHandles())
+        #driver.switchTo().window()
+        #driver.get(next_page)
+        #vacancy_cliks(next_page)
+
+        driver.close()
