4cf0300e643b3b35d7e40eb3957a33797e84e934
Akami14 <98305306+Akami14@users.noreply.github.com>
2025-06-30 16:33:52 +0300

    Add files via upload

--- hhru_func_2.py
+++ hhru_func_2.py
@@ -15,35 +15,37 @@ def loggin_and_click(driver, login, url):
     ActionChains(driver).click(driver.find_element(By.XPATH, "//*[@value='50']")).perform()
 
     driver.get(url=url)
-    driver.implicitly_wait(40)
+    driver.implicitly_wait(90)
 
     driver.find_element(By.XPATH, '//div/div/div[1]/div[3]/div[1]/div/div/div/div[6]/a').click() #/
     driver.find_element(By.XPATH,'//div[1]/div/div/div[1]/div/div/div/div/div/div[1]/div/div/form/div/div[3]/button[1]').click()
-    #driver.find_element(By.XPATH, '//div/div/div[1]/div/div/div/div/div/div[1]/div/div/form/div/div/div[2]/div/label[2]/div/div/div').click()
+
     sleep(0.5)
-    a = driver.find_element(By.CLASS_NAME,"magritte-field___9S8Tw_7-2-13")
+    #a = driver.find_element(By.XPATH,'//div/div[1]/div/div/div[1]/div/div/div/div/div/div[1]/div/div/form/div/div/div[3]/div[1]/div/div[2]/div[2]/input')
+    a = driver.find_element(By.CLASS_NAME,"magritte-field___9S8Tw_7-2-20") # xh-highlight
+
     ActionChains(driver).move_to_element(a).click(a).send_keys(login).perform()
-    sleep(40) # cause we waiting sms
+    sleep(5.40) # cause we waiting sms
     while True:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
         driver.implicitly_wait(9)
         list_of_vacancy = driver.find_elements(By.XPATH, '//div[1]/div/div/div/div[2]/div[2]/div/a/div/span/span').copy() #    //div[1]/div/div/div/div[2]/div[2]/div/a'   //div[1]/div/div/div/div[2]/div[2]/div/a/div/span/span[contains(@class, "magritte-button__label___zplmt")]#'//a[@class = "bloko-button bloko-button_kind-primary"]/span'
         driver.execute_script("window.scrollTo(document.body.scrollHeight, 0)")
-
+        range_of_list = len(list_of_vacancy)
         print(len(list_of_vacancy))
         for i, el in enumerate(list_of_vacancy):
             print(i, el)
-            sleep(1.5)
+            driver.execute_script('arguments[0].scrollIntoView()', el)
             ActionChains(driver).move_to_element(el).click(el).perform()
-            sleep(1.5)
-            # el.click()
-
-
+            sleep(0.5)
+            #just click
             try:
                 sleep(1.05)
-                ActionChains(driver).move_to_element(el).click(el)
+                b1 = driver.find_element(By.XPATH, '//button[contains(@data-qa, "vacancy-response-submit-popup")]/div/span/span')
+                ActionChains(driver).move_to_element(b1).click(b1).perform()
             except (NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException, ElementNotInteractableException):
                 print('E1')
+
 
 
             try: ## просто отклик
@@ -54,25 +56,35 @@ def loggin_and_click(driver, login, url):
             except (NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException):
                 print('E3, just click on vacancy')
 
+            """   
+            try:
+                sleep(.5)
+                driver.find_element(By.XPATH,
+                              '//button[contains(@data-qa,"relocation-warning-abort")]').click()  # отклик в другой стране
 
-            try: ## ост случаи
-                el.find_element(By.XPATH,'//button[contains(@data-qa,"response-popup-close")]').click()
+                ActionChains(driver).click(driver.find_element(By.XPATH, '/button[contains(@data-qa,"relocation-warning-abort")]')).perform()
+
+          except (NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException):
+              print('E2')
+
+
+          try: ## ост случаи
+              el.find_element(By.XPATH,'//button[contains(@data-qa,"response-popup-close")]').click()
+          except (NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException):
+              print('E4 close x')
+              """
+
+            try:
+                crosser = driver.find_element(By.XPATH, '/html/body/div[154]/div/div[1]/div[1]/div/div[2]/span/div/button/span')
+                crosser.click()
             except (NoSuchElementException, ElementClickInterceptedException, StaleElementReferenceException):
-                print('E4 close x')
+                print('E5 close x')
 
-
-
-
-
-    #ActionChains(driver).move_to_element(el).click(el)
-            sleep(1.5)
-            #response = driver.find_element(By.XPATH, "//div/div[3]/div/div/div/div/div[2]/button/div/span/span[contains(@class, 'magritte-button__label___zplmt_5-3-10')]")
-            #ActionChains(driver).move_to_element(response).click(response)
-
+            #driver.execute_script("window.scrollBy(0, 900)")
 
             if 'applicant' in driver.current_url.split('/'):
                 driver.back()
-            driver.execute_script("window.scrollBy(0, 800)")
+
 
 
 
